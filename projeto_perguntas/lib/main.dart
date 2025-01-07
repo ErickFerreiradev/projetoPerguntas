17a61b6c377cb77e293f78b6ae2b97dd77345a03
Erick Ferreira <erickferreiradev@gmail.com>
2025-01-07 01:07:28 -0300

    Novo projeto iniciado

---
+++ projeto_perguntas/lib/main.dart
@@ -0,0 +1,49 @@
+
+import 'package:flutter/material.dart';
+
+main () {
+  runApp(PerguntaApp());
+}
+
+
+class PerguntaApp extends StatelessWidget {
+
+  void responder() {
+    print('Pergunta respondida!');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
+
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas.elementAt(0)),
+            ElevatedButton(
+              child: Text('Resposta 1'),
+              onPressed: responder,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 2'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 3'),
+              onPressed: null,
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
